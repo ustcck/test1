7664f86fc97ffd12c2de9ac654c06e09c15c3bfb
ustcck <ustcck@outlook.com>
2016-12-04 21:24:34 +0800

    Initialize Project

---
+++ src/Hello.java
@@ -0,0 +1,18 @@
+/**
+ * Created by IntelliJ IDEA
+ * Created by ustcck on 2016/12/4 19:13.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello james");
+        System.out.println("hello james");
+        System.out.println("hello tom");
+        System.out.println("hello tom");
+        System.out.println("hello tom");
+        System.out.println("hello lily");
+        System.out.println("hello lily");
+        System.out.println("hello lily");
+        System.out.println("hello lily");
+        System.out.println("hello lily");
+    }
+}
